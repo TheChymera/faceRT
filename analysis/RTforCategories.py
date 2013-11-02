ac1f23df63aa2ffcf3252d23b024aa41d7b7e8a3
Horea Christian <h.chr@mail.ru>
2013-11-02 22:02:10 +0100

    Added dynamic spacing to position individual-to-total separator properly regardless of humber of scrambling trials.

--- analysis/RTforCategories.py
+++ analysis/RTforCategories.py
@@ -9,13 +9,13 @@ from pylab import figure, show, errorbar, setp, legend
 from matplotlib import axis
 from data_functions import get_and_filter_results
 
-def main(experiment=False, source=False, prepixelation=False):
+def main(experiment=False, source=False, prepixelation=False, elinewidth=2, ecolor='0.4', make_tight=True):
 	data_all = get_and_filter_results(experiment, source, prepixelation)
 	
 	ids = sorted(list(set(data_all['ID'])))
 	pos_ids = np.arange(len(ids))
 	
-	fig = figure(figsize=(pos_ids.max()*4, 5), dpi=80,facecolor='#eeeeee',tight_layout=True)
+	fig = figure(figsize=(pos_ids.max()*5, 4), dpi=300,facecolor='#00eeee', tight_layout=make_tight)
 	ax=fig.add_subplot(1,1,1)
 	width = 0.1
 	ax.yaxis.grid(True, linestyle='-', which='major', color='#dddddd',alpha=0.5, zorder = 1)
@@ -26,38 +26,42 @@ def main(experiment=False, source=False, prepixelation=False):
 	for scrambling_id, scrambling in enumerate(set(data_all['scrambling'])):
 		if scrambling == 0:
 			#below this: per-participant graphs
-			plot_em_strong = plt.bar(pos_ids-width, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)].groupby('ID')['RT'].mean(), width ,color='m', alpha=0.4, zorder = 1)
-			errorbar(pos_ids-(width/2), data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)].groupby('ID')['RT'].mean(), yerr=data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].aggregate(sem), ecolor='0.5', elinewidth='3', capsize=0, linestyle='None', zorder = 2)
-			plot_em_weak = plt.bar(pos_ids, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)].groupby('ID')['RT'].mean(), width ,color='m', alpha=0.7, zorder = 1)
-			errorbar(pos_ids+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)].groupby('ID')['RT'].mean(), yerr=data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].aggregate(sem), ecolor='0.5', elinewidth='3', capsize=0, linestyle='None', zorder = 2)
+			plot_em_strong = plt.bar(pos_ids-width, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)].groupby('ID')['RT'].mean(), width ,color='m', alpha=0.4, zorder = 1, linewidth=0)
+			errorbar(pos_ids-(width/2), data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)].groupby('ID')['RT'].mean(), yerr=data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].aggregate(sem), ecolor=ecolor, elinewidth=elinewidth, capsize=0, linestyle='None', zorder = 2)
+			plot_em_weak = plt.bar(pos_ids, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)].groupby('ID')['RT'].mean(), width ,color='m', alpha=0.7, zorder = 1, linewidth=0)
+			errorbar(pos_ids+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)].groupby('ID')['RT'].mean(), yerr=data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].aggregate(sem), ecolor=ecolor, elinewidth=elinewidth, capsize=0, linestyle='None', zorder = 2)
 			#below this: total graphs
-			plt.bar(pos_ids[-1]+1-width, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)]['RT'].mean(), width ,color='m', alpha=0.4, zorder = 1)
-			errorbar(pos_ids[-1]+1-(width/2), data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)]['RT'].mean(), yerr=sem(data_all[(data_all['scrambling'] == scrambling)]['RT']), ecolor='0.5', elinewidth='3', capsize=0, linestyle='None', zorder = 2)
-			plt.bar(pos_ids[-1]+1, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)]['RT'].mean(), width ,color='m', alpha=0.7, zorder = 1)
-			errorbar(pos_ids[-1]+1+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)]['RT'].mean(), yerr=sem(data_all[(data_all['scrambling'] == scrambling)]['RT']), ecolor='0.5', elinewidth='3', capsize=0, linestyle='None', zorder = 2)
+			plt.bar(pos_ids[-1]+1-width, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)]['RT'].mean(), width ,color='m', alpha=0.4, zorder = 1, linewidth=0)
+			errorbar(pos_ids[-1]+1-(width/2), data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 100)]['RT'].mean(), yerr=sem(data_all[(data_all['scrambling'] == scrambling)]['RT']), ecolor=ecolor, elinewidth=elinewidth, capsize=0, linestyle='None', zorder = 2)
+			plt.bar(pos_ids[-1]+1, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)]['RT'].mean(), width ,color='m', alpha=0.7, zorder = 1, linewidth=0)
+			errorbar(pos_ids[-1]+1+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling) & (data_all['intensity'] == 40)]['RT'].mean(), yerr=sem(data_all[(data_all['scrambling'] == scrambling)]['RT']), ecolor=ecolor, elinewidth=elinewidth, capsize=0, linestyle='None', zorder = 2)
 		else:
 			#below this: per-participant graphs
-			plot_sc = plt.bar(pos_ids+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].mean(), width ,color='g', alpha=0.2+0.12*scrambling_id, zorder = 1)
-			errorbar(pos_ids+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].mean(), yerr=data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].aggregate(sem), ecolor='0.5', elinewidth='3', capsize=0, linestyle='None', zorder = 2)
+			plot_sc = plt.bar(pos_ids+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].mean(), width ,color='g', alpha=0.2+0.12*scrambling_id, zorder = 1, linewidth=0)
+			errorbar(pos_ids+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].mean(), yerr=data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].aggregate(sem), ecolor=ecolor, elinewidth=elinewidth, capsize=0, linestyle='None', zorder = 2)
 			#below this: total graphs
-			plot_sc = plt.bar(pos_ids[-1]+1+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)]['RT'].mean(), width ,color='g', alpha=0.2+0.12*scrambling_id, zorder = 1)
-			errorbar(pos_ids[-1]+1+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)]['RT'].mean(), yerr=sem(data_all[(data_all['scrambling'] == scrambling)]['RT']), ecolor='0.5', elinewidth='3', capsize=0, linestyle='None', zorder = 2)
+			plot_sc = plt.bar(pos_ids[-1]+1+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)]['RT'].mean(), width ,color='g', alpha=0.2+0.12*scrambling_id, zorder = 1, linewidth=0)
+			errorbar(pos_ids[-1]+1+(width/2)+width*scrambling_id, data_all[(data_all['scrambling'] == scrambling)]['RT'].mean(), yerr=sem(data_all[(data_all['scrambling'] == scrambling)]['RT']), ecolor=ecolor, elinewidth=elinewidth, capsize=0, linestyle='None', zorder = 2)
 	
-	plt.axvline(pos_ids[-1]+1-width*2.5, color='0.2')
+	width_multiplier = 15/np.shape(data_all[(data_all['scrambling'] == scrambling)].groupby('ID')['RT'].mean())[0]
+	plt.axvline(pos_ids[-1]+1-width*width_multiplier, color='0.2')
 	
 	
 	ids=ids+['TOTAL']
 	pos_ids = np.arange(len(ids))
-	ax.set_xlim(0, pos_ids[-1]+width*5)
-	ax.set_ylabel(r'$\mathsf{\overline{RT}}$ [s]', fontsize=13)
-	ax.set_xlabel('Participant')
+	ax.set_ylabel(r'$\mathsf{\overline{RT}}$ [s]', fontsize=11)
+	ax.set_xlabel('Participant', fontsize=11)
 	ax.set_xticks(pos_ids + width*3)
-	ax.set_xticklabels(ids,fontsize=9,rotation=30)
+	ax.set_xticklabels(ids,fontsize=9) # add rotation=30 if things get too crowded
 	for tick in ax.axes.get_xticklines():
 		tick.set_visible(False)
-	axis.Axis.zoom(ax.xaxis, -0.5)
-	legend((plot_em_strong,plot_em_weak, plot_sc),('Strong Emotion','Weak Emotion', 'Scrambled '+', '.join(scrambling_list)), 'upper right', shadow=False, frameon=False, prop= FontProperties(size='11'))
+	ax.set_xlim(0, pos_ids[-1]+width*5) # before scaling to add padding in front of zero
+	axis.Axis.zoom(ax.xaxis, -0.5) # sets x margins further apart from the content proportional to its length
+	axis.Axis.zoom(ax.yaxis, -0.5) # sets y margins further apart from the content proportional to its length
+	ax.set_ylim(bottom=0) # after scaling to disregard padding unerneath zero.
+	legend((plot_em_strong,plot_em_weak, plot_sc),('Strong Emotion','Weak Emotion', 'Scrambled '+', '.join(scrambling_list)),loc='upper center', bbox_to_anchor=(0.5, 1.065), ncol=3, fancybox=False, shadow=False,prop= FontProperties(size='9'))
+	#~ legend((plot_em_strong,plot_em_weak, plot_sc),('Strong Emotion','Weak Emotion', 'Scrambled '+', '.join(scrambling_list)), 'upper right', shadow=False, frameon=False, prop= FontProperties(size='11'))
 
 if __name__ == '__main__':
-	main()
+	main(experiment='11px-4px-5steps', prepixelation=6)
 	show()
